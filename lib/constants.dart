dd30c1da113ace2a7633f1b1484cdc8d50db2d19
Kevoh <40100329+kev254@users.noreply.github.com>
2023-01-29 09:32:10 +0300

    initial commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-String kParseApplicationId= "yy1cOOpOW0Wb3oNjNevTn9AGD2YHdhFkddbn3Bzq";
-String kParseClientKey = "TLNtAJUYXmIUaI4CP2RMwcTRsVrUtD6l5AF3anXq";
-String kParseMasterKey = "u7LfktDcarT88Wb71w4RV9LUTw9VoeaIYoM86w81";
+String kParseApplicationId= "";
+String kParseClientKey = "";
+String kParseMasterKey = "";
